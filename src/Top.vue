8081cf9257573f9981555cf97e0daf7dc5233715
张红亮 <zhanghongliang@zhanghoiangsAir.lan>
2019-08-28 18:48:50 +0800

    top

---
+++ src/Top.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-container>
+    <router-view></router-view>
+    <div ref="scrollTarget" class="test-scroll" style="height: 100px; overflow: auto">
+      <div style="height: 10000px; width: 100%">
+        scoll to see the backtop button
+        <el-backtop target=".test-scroll">
+          <span>test_up_text</span>
+        </el-backtop>
+      </div>
+    </div>
+  </el-container>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import ElementUI from "element-ui";
 // import "./element-variables.scss";
 import "element-ui/lib/theme-chalk/index.css";
+import Top from "./Top.vue";
 import App from "./App.vue";
 import Nuxt from "./Nuxt.vue";
 import Grid from "./Grid.vue";
@@ -13,31 +14,20 @@ import Transition from "./Transition.vue";
 import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
-// 0. If using a module system (e.g. via vue-cli), import Vue and VueRouter
-// and then call `Vue.use(VueRouter)`.
-
-// 1. Define route components.
-// These can be imported from other files
-// const Foo = { template: Transition };
-// const Bar = { template: Nuxt };
-
-// 2. Define some routes
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// `Vue.extend()`, or just a component options object.
-// We'll talk about nested routes later.
 const routes = [
-  { path: "/", component: App },
-  { path: "/nuxt", component: Nuxt },
-  { path: "/grid", component: Grid },
-  { path: "/transition", component: Transition },
-  { path: "/tree", component: Tree },
-  { path: "/form", component: Form }
+  {
+    path: "/",
+    component: Top,
+    children: [
+      { path: "/app", component: App },
+      { path: "/nuxt", component: Nuxt },
+      { path: "/grid", component: Grid },
+      { path: "/transition", component: Transition },
+      { path: "/tree", component: Tree },
+      { path: "form", component: Form }
+    ]
+  }
 ];
-
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = new VueRouter({
   routes // short for `routes: routes`
 });
@@ -46,13 +36,6 @@ const router = new VueRouter({
 // Make sure to inject the router with the router option to make the
 // whole app router-aware.
 Vue.use(ElementUI);
-// new Vue({ el: "#app", router, render: h => h(App) });
-
-// Vue.component(Button.name, Button);
-// Vue.component(Select.name, Select);
-// Vue.component(Dialog.name, Dialog);
-// Vue.component(Notification.name, Notification);
-
 Vue.use(Dialog, { size: "small", zIndex: 3000 });
 const app = new Vue({
   router
